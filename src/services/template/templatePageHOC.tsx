0cc46bbf6e8eb3d5a6cdb526b965e0e8c0408a68
Marco <mbarozzi@gmail.com>
2024-10-01 01:32:22 -0300

    refatorando props title page

--- src/services/template/templatePageHOC.tsx
+++ src/services/template/templatePageHOC.tsx
@@ -1,26 +1,26 @@
 import Head from "next/head";
-import type {TemplateConfig} from "./whitTemplateConfig" ;
+import type { TemplateConfig } from "./whitTemplateConfig";
 interface templatePageHOCProps {
   title?: string;
 }
 
 export default function templatePageHOC(
-  Componet:(props:any)=> JSX.Element,
+  Componet: (props: any) => JSX.Element,
   templatePageHOCProps: templatePageHOCProps = {}
 ) {
-  return function WrappedComponent(props:{templateConfig:TemplateConfig}) {
+  return function WrappedComponent(props: { templateConfig: TemplateConfig }) {
     console.log(props);
     return (
       <>
-      <Head>
-        <title>
-          {props.templateConfig.site.title}
-        </title>
-      </Head>
-      <Componet {...props}/>
+        <Head>
+          <title>
+            {templatePageHOCProps?.title
+              ? `${templatePageHOCProps?.title} | ${props.templateConfig.site.title}`
+              : props.templateConfig.site.title}
+          </title>
+        </Head>
+        <Componet {...props} />
       </>
-    )
-
-
+    );
   };
 }
