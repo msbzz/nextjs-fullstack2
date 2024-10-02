5943d2c2b8fb32d9e627a100337154088a43498b
Marco <mbarozzi@gmail.com>
2024-10-02 05:48:16 -0300

    lendo postagens parte 2

--- src/services/posts/PostsService.ts
+++ src/services/posts/PostsService.ts
@@ -16,7 +16,13 @@ export default function PostsService() {
   return {
     async getAll(): Promise<Post[]> {
       const PATH_POSTS= path.resolve(".","_data","posts");
-      console.log('lendo posts ==>>',PATH_POSTS)
+      const postFiles = await fs.readdir(PATH_POSTS,{encoding:"utf-8"});
+      const posts = postFiles.map(async (postFileName)=>{
+        const filePath = path.join(PATH_POSTS,postFileName);
+        const postFile = await fs.readFile(filePath,{encoding:"utf-8"});
+        console.log('lendo posts ==>>',postFile);
+      });
+
       return [];
     },
   };
