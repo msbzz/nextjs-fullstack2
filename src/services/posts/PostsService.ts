00351657fff97bc156ba8d40e00a7bbd6418b39d
Marco <mbarozzi@gmail.com>
2024-10-02 06:29:31 -0300

    lendo posts final

--- src/services/posts/PostsService.ts
+++ src/services/posts/PostsService.ts
@@ -1,32 +1,46 @@
-
 import path from "path";
 import fs from "fs/promises";
 import matter from "gray-matter";
 
-export interface Post{
-  date : string;
-  slug: string // ID do post / titlulo resumido
+export interface Post {
+  metadata: {
+    date: string;
+    url: string;
+    excerpt: string;
+    tags: string[];
+  };
+  slug: string; // ID do post / titlulo resumido
   title: string;
-  url: string;
-  excerpt: string;
-  tags: string[];
+  content: string;
 }
-
 
 export default function PostsService() {
   return {
     async getAll(): Promise<Post[]> {
-      const PATH_POSTS= path.resolve(".","_data","posts");
-      const postFiles = await fs.readdir(PATH_POSTS,{encoding:"utf-8"});
-      const posts = postFiles.map(async (postFileName)=>{
-        const filePath = path.join(PATH_POSTS,postFileName);
-        const postFile = await fs.readFile(filePath,{encoding:"utf-8"});
-        const {data,content} = matter(postFile);
-        console.log('dados ==>>',data);
-        console.log('conteudo ==>>',content);
-      });
+      const PATH_POSTS = path.resolve(".", "_data", "posts");
+      const postFiles = await fs.readdir(PATH_POSTS, { encoding: "utf-8" });
+      const postsPromise = postFiles.map(async (postFileName) => {
+        const filePath = path.join(PATH_POSTS, postFileName);
+        const postFile = await fs.readFile(filePath, { encoding: "utf-8" });
+        const { data, content } = matter(postFile);
 
-      return [];
+        const post:Post = {
+          metadata:{
+             date :data.date.toString(),
+             url:data.url,
+             excerpt:data.excerpt,
+             tags:data.tags,
+          },
+          slug:postFileName.replace(".md",""),
+          title:data.title,
+          content,
+        }
+        console.log('item array ',post)
+        return post
+      });
+      const posts = Promise.all(postsPromise);
+      console.log('array posts',posts);
+      return posts;
     },
   };
 }
