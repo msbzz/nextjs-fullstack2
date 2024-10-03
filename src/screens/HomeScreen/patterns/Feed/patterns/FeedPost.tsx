2d32d8981a063a906fdb4ade2774620892463596
Marco <mbarozzi@gmail.com>
2024-10-03 01:44:38 -0300

    feedPost title

--- src/screens/HomeScreen/patterns/Feed/patterns/FeedPost.tsx
+++ src/screens/HomeScreen/patterns/Feed/patterns/FeedPost.tsx
@@ -36,12 +36,40 @@ export function FeedPost({ title, excerpt, date, tags, url }: FeedPostProps) {
       {/* Date  */}
       <Text
         variant="body4"
-        styleSheet={{ fontWeight: 'bold', marginBottom: '32px', marginLeft: '4px' }}
+        styleSheet={{
+          fontWeight: "bold",
+          marginBottom: "32px",
+          marginLeft: "4px",
+        }}
       >
         {postDate}
       </Text>
-      {/* title  */}
-       {title}
+      <Link
+        href={url}
+        variant="body1"
+        styleSheet={{
+          marginBottom: "8px",
+          display: "inline",
+        }}
+        colorVariantEnabled={false}
+      >
+        <Box
+          tag="span"
+          styleSheet={{
+            display: "inline",
+            padding: "2px",
+            borderRadius: "4px",
+            color: theme.colors.neutral.x800,
+            backgroundColor: theme.colors.neutral.x200,
+            hover: {
+              color: theme.colors.primary.x900,
+              backgroundColor: theme.colors.primary.x200,
+            },
+          }}
+        >
+          {title}
+        </Box>
+      </Link>
     </Box>
   );
 }
@@ -79,7 +107,6 @@ function FeedPostSideTimeline() {
           position: "absolute",
           width: "1px",
           backgroundColor: "currentColor",
-
         }}
       />
     </Box>
